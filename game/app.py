ad0497be8cbc698a72297a0c16c9d0a5252e3875
Ivan Gibert <vngbrt@gmail.com>
2023-02-02 21:04:53 +0300

    Edited app.py

--- game/app.py
+++ game/app.py
@@ -10,4 +10,7 @@ tk.wm_attributes('-topmost', 1)
 canvas = Canvas(tk, width=500, height=400, highlightthickness=0)
 canvas.pack()
 
-tk.update()
+while not False:
+    tk.update_idletasks()
+    tk.update()
+    time.sleep(0.01)
